77a43a44f55b88b7ba8fa0a0e1d372245d3ab519
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-04-16 11:00:41 -0300

    acertos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,8 +3,6 @@ import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
 import { MainTemplate } from "../../templates/MainTemplate";
 
-
-
 export function Home() {
   return (
     <MainTemplate>
--- src/templates/MainTemplate/index.tsx
+++ src/templates/MainTemplate/index.tsx
@@ -1,8 +1,6 @@
 import { Container } from '../../components/Container';
-import { CountDown } from '../../components/CountDown';
 import { Footer } from '../../components/Footer';
 import { Logo } from '../../components/Logo';
-import { MainForm } from '../../components/MainForm';
 import { Menu } from '../../components/Menu';
 
 type MainTemplateProps = {
