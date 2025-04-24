bc615dbf911804b1c03cfd6ca27a49e01bf0df48
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-04-24 18:34:07 -0300

    aula 45

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './styles/global.css';
 import { useState } from 'react';
 import { TaskStateModel } from './models/TaskStateModel';
 
-const initialState: TaskStateModel  = {
+const initialState: TaskStateModel = {
   tasks: [],
   secondsRemaining: 0,
   formattedSecondsRemaining: '00:00',
@@ -14,28 +14,25 @@ const initialState: TaskStateModel  = {
   config: {
     workTime: 25,
     shortBreakTime: 5,
-    longBreakTime:15,
+    longBreakTime: 15,
   },
 };
 
 export function App() {
   const [state, setState] = useState(initialState);
 
-  return <Home state={state} setState={setState} />
-
+  return <Home />;
 }
 
+// sempre que eu usar useState, eu vou ter que importar
+// useState é um hook do react que serve para criar estados
+// useState retorna um array com dois elementos
+// o primeiro elemento é o valor do estado e o segundo elemento é uma função para atualizar o estado
+// o valor do estado pode ser de qualquer tipo, string, number, boolean, array, object, etc
+// o segundo elemento é uma função que recebe um novo valor para o estado
+// e atualiza o estado com esse novo valor
+// o valor inicial do estado é passado como argumento para o useState
+// o valor inicial do estado pode ser de qualquer tipo, string, number, boolean, array, object, etc
+// o valor inicial do estado pode ser passado como argumento para o useState
 
-  // sempre que eu usar useState, eu vou ter que importar
-  // useState é um hook do react que serve para criar estados
-  // useState retorna um array com dois elementos   
-  // o primeiro elemento é o valor do estado e o segundo elemento é uma função para atualizar o estado
-  // o valor do estado pode ser de qualquer tipo, string, number, boolean, array, object, etc   
-  // o segundo elemento é uma função que recebe um novo valor para o estado
-  // e atualiza o estado com esse novo valor
-  // o valor inicial do estado é passado como argumento para o useState
-  // o valor inicial do estado pode ser de qualquer tipo, string, number, boolean, array, object, etc
-  // o valor inicial do estado pode ser passado como argumento para o useState
-
-  // sempre que eu usar useState, não vou usar atribuição diretamente
-
+// sempre que eu usar useState, não vou usar atribuição diretamente
--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,32 +2,10 @@ import { PlayCircleIcon } from 'lucide-react';
 import { Cycles } from '../Cycles';
 import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
-import { HomeProps } from '../../pages/Home';
 
-
-export function MainForm({ state, setState }: HomeProps) {
-
-  function handleClick() {
-    setState(prevState => {
-      return {
-        ...prevState,
-        config: {
-          ...prevState.config,
-          workTime: 34,
-        },
-        formattedSecondsRemaining: '23:34',
-      };
-    });
-  }
-
-
+export function MainForm() {
   return (
     <form className='form' action=''>
-
-      <div>
-        <button type='button' onClick={handleClick}> Clicar </button>
-      </div>
-
       <div className='formRow'>
         <DefaultInput
           labelText='task'
@@ -38,7 +16,7 @@ export function MainForm({ state, setState }: HomeProps) {
       </div>
 
       <div className='formRow'>
-        <p>Próximo itervalo é de {state.config.workTime} min.</p>
+        <p>Próximo itervalo é de 25 min.</p>
       </div>
 
       <div className='formRow'>
