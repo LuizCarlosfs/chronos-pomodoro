3f2ebab449b23c187ce73e614915adf4c939d6f9
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-04-24 14:28:57 -0300

    Aula 44

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,31 +1,53 @@
 import { PlayCircleIcon } from 'lucide-react';
- import { Cycles } from '../Cycles';
- import { DefaultButton } from '../DefaultButton';
- import { DefaultInput } from '../DefaultInput';
- 
- export function MainForm() {
-   return (
-     <form className='form' action=''>
-       <div className='formRow'>
-         <DefaultInput
-           labelText='task'
-           id='meuInput'
-           type='text'
-           placeholder='Digite algo'
-         />
-       </div>
- 
-       <div className='formRow'>
-         <p>Lorem ipsum dolor sit amet.</p>
-       </div>
- 
-       <div className='formRow'>
-         <Cycles />
-       </div>
- 
-       <div className='formRow'>
-         <DefaultButton icon={<PlayCircleIcon />} />
-       </div>
-     </form>
-   );
- }
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+import { HomeProps } from '../../pages/Home';
+
+
+export function MainForm({ state, setState }: HomeProps) {
+
+  function handleClick() {
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: {
+          ...prevState.config,
+          workTime: 34,
+        },
+        formattedSecondsRemaining: '23:34',
+      };
+    });
+  }
+
+
+  return (
+    <form className='form' action=''>
+
+      <div>
+        <button type='button' onClick={handleClick}> Clicar </button>
+      </div>
+
+      <div className='formRow'>
+        <DefaultInput
+          labelText='task'
+          id='meuInput'
+          type='text'
+          placeholder='Digite algo'
+        />
+      </div>
+
+      <div className='formRow'>
+        <p>Próximo itervalo é de {state.config.workTime} min.</p>
+      </div>
+
+      <div className='formRow'>
+        <Cycles />
+      </div>
+
+      <div className='formRow'>
+        <DefaultButton icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,29 @@
-import { Container } from "../../components/Container";
-import { CountDown } from "../../components/CountDown";
-import { MainForm } from "../../components/MainForm";
-import { MainTemplate } from "../../templates/MainTemplate";
+import React from 'react';
+import { Container } from '../../components/Container';
+import { CountDown } from '../../components/CountDown';
+import { MainForm } from '../../components/MainForm';
+import { TaskStateModel } from '../../models/TaskStateModel';
+import { MainTemplate } from '../../templates/MainTemplate';
 
-export function Home() {
+export type HomeProps = {
+  state: TaskStateModel;
+  setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
+};
+
+export function Home(props: HomeProps) {
+  // useEffect(() => {
+  //   document.title = 'Chronos Pomodoro';
+  // }, []);
+
   return (
     <MainTemplate>
       <Container>
-        <CountDown />
+        <CountDown {...props} />
       </Container>
 
       <Container>
-        <MainForm />
+        <MainForm {...props} />
       </Container>
-   
     </MainTemplate>
   );
 }
