cbe56ade163d32c7ccca8aa1b9b663483feb2e54
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-04-28 15:56:21 -0300

    Aula 53

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -5,19 +5,54 @@ import { DefaultInput } from '../DefaultInput';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 //import { useState } from 'react';
 import { useRef } from 'react';
+import { TaskModel } from '../../models/TaskModel';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+
   //const [taskName, setTaskName] = useState('');
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('DEU CERTO');
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('Digite o nome da tarefa');
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1, // conferir
+        secondsRemaining,   // conferir
+        formatedSecondsRemaining: '00.00',  // conferir
+        task: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
-        <div className='formRow'>
+      <div className='formRow'>
         <DefaultInput
           labelText='task'
           id='meuInput'
